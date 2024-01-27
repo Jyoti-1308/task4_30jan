05727ef16c58e9c8098a49c5885c9cb14083a767
Jyoti_Parashar2000 <parasharjyoti2000@gmail.com>
2024-01-27 12:53:17 +0530

    first commit

--- NDB15task3-2.js
+++ NDB15task3-2.js
@@ -25,32 +25,62 @@ app.post("/myServer", function (req, res) {
     console.log(body);
     if (body.method === "POST") {
         // let data2=JSON.parse(body.data1);
-        axios.post(body.fetchURL, body.data)
-            .then(function (response) {
-                // console.log(response);
-                res.send(response);
+        if (body.fetchURL === (baseUrl + "/students")) {
+            console.log(body.header);
+            let value = body.header.find(ele=>ele.key1.toLowerCase()==="authorization").value1;
+            if (!value)
+                res.sendStatus(401).send({ err_code: 401, err_message: "No token found. Provide a valid token" });
+            else {
+                let body = req.body;
+                // let newJson={id:body};
+                console.log(body.data);
+                axios.post(baseUrl + "/students", body.data, { headers: { authorization: value } })
+                    .then(function (response) {
+                        // console.log(response);
+                        res.send(response.data);
+                    })
+                    .catch(function (err) {
+                        if (err.response) {
+                            // console.log(err.response);
+                            let { status, statusText } = err.response;
+                            console.log(status, statusText);
+                            let json = { err_code: status, err_message: statusText };
+                            console.log(json);
+                            res.sendStatus(401).send(json);
+                        }
+                    });
+            }
+        }
+        else {
+            axios.post(body.fetchURL, body.data)
+                .then(function (response) {
+                    // console.log(response);
+                    res.send(response.data);
 
-            })
-            .catch(function (err) {
-                if (err.response) {
-                    // console.log(err.response);
-                    let { status, statusText } = err.response;
-                    console.log(status, statusText);
-                    let json = { err_code: status, err_message: statusText };
-                    console.log(json);
-                    res.status(401).send(json);
-                }
-            });
+                })
+                .catch(function (err) {
+                    if (err.response) {
+                        // console.log(err.response);
+                        let { status, statusText } = err.response;
+                        console.log(status, statusText);
+                        let json = { err_code: status, err_message: statusText };
+                        console.log(json);
+                        res.sendStatus(401).send(json);
+                    }
+                });
+        }
     }
     if (body.method === "GET") {
         if (body.fetchURL === (baseUrl + "/students")) {
-            console.log(body.key1);
-            let value = body.key1 && (body.key1.toLowerCase() === "authorization") ? body.value1 : "";
-
+            console.log(body.header);
+            let value = body.header.find(ele=>ele.key1.toLowerCase()==="authorization").value1;
+            if (!value)
+                res.sendStatus(401).send({ err_code: 401, err_message: "No token found. Provide a valid token" });
+            else {
             axios.get(body.fetchURL, { headers: { authorization: value } })
                 .then(function (response) {
                     console.log(response);
-                    res.send(response);
+                    res.send(response.data);
                 })
                 .catch(function (err) {
                     if (err.response) {
@@ -58,47 +88,46 @@ app.post("/myServer", function (req, res) {
                         console.log(status, statusText);
                         let json = { err_code: status, err_message: statusText };
                         console.log(json);
-                        res.status(401).send(json);
+                        res.sendStatus(401).send(json);
                     }
                 });
+            }
         }
-        else 
-        if (body.fetchURL === (baseUrl + "/getToken")) {
-            console.log("inside base url get token");
-            axios.get(body.fetchURL)
-                .then(function (response) {
+        else
+            if (body.fetchURL === (baseUrl + "/getToken")) {
+                console.log("inside base url get token");
+                axios.get(body.fetchURL)
+                    .then(function (response) {
+                        res.send(response.data);
+                    })
+                    .catch(function (err) {
+                        if (err.response) {
+                            let { status, statusText } = err.response;
+                            console.log(status, statusText);
+                            let json = { err_code: status, err_message: statusText };
+                            console.log(json);
+                            res.sendStatus(401).send(json);
+                        }
+                    });
+            }
+            else {
+                axios.get(body.fetchURL)
+                    .then(function (response) {
+                        // console.log("inside get url axios");
+                        // console.log(response.data);
+                        res.send(response.data);
 
-                    console.log(response);
-                    res.send(response);
-                })
-                .catch(function (err) {
-                    if (err.response) {
-                        let { status, statusText } = err.response;
-                        console.log(status, statusText);
-                        let json = { err_code: status, err_message: statusText };
-                        console.log(json);
-                        res.status(401).send(json);
-                    }
-                });
-        }
-        else {
-            axios.get(body.fetchURL)
-                .then(function (response) {
-                    // console.log("inside get url axios");
-                    // console.log(response.data);
-                    res.send(response);
-
-                })
-                .catch(function (err) {
-                    if (err.response) {
-                        let { status, statusText } = err.response;
-                        console.log(status, statusText);
-                        let json = { err_code: status, err_message: statusText };
-                        console.log(json);
-                        res.status(401).send(json);
-                    }
-                });
-        }
+                    })
+                    .catch(function (err) {
+                        if (err.response) {
+                            let { status, statusText } = err.response;
+                            console.log(status, statusText);
+                            let json = { err_code: status, err_message: statusText };
+                            console.log(json);
+                            res.sendStatus(401).send(json);
+                        }
+                    });
+            }
     }
 
 })
