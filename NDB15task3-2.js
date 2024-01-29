305121752338631ebc98d88692791de361d6b826
Jyoti_Parashar2000 <parasharjyoti2000@gmail.com>
2024-01-29 13:30:37 +0530

    todays commit

--- NDB15task3-2.js
+++ NDB15task3-2.js
@@ -24,29 +24,36 @@ app.post("/myServer", function (req, res) {
     let body = req.body;
     console.log(body);
     if (body.method === "POST") {
-        // let data2=JSON.parse(body.data1);
         if (body.fetchURL === (baseUrl + "/students")) {
-            console.log(body.header);
-            let value = body.header.find(ele=>ele.key1.toLowerCase()==="authorization").value1;
-            if (!value)
-                res.sendStatus(401).send({ err_code: 401, err_message: "No token found. Provide a valid token" });
+            let token = req.header("authorization");
+           
+            let ele = body.header.find(ele => {
+                if (ele.key) {
+                    console.log(ele.key, ele.value);
+                    return ele.key.toLowerCase() === "authorization";
+                }
+            });
+            if (!ele.value)
+                res.status(401).send({ err_code: 401, err_message: "No token found. Provide a valid token" });
             else {
                 let body = req.body;
                 // let newJson={id:body};
                 console.log(body.data);
-                axios.post(baseUrl + "/students", body.data, { headers: { authorization: value } })
+                axios.post(baseUrl + "/students", body.data, { headers: { authorization: token } })
                     .then(function (response) {
-                        // console.log(response);
-                        res.send(response.data);
+                        // console.log(response.data);
+                        console.log("inside post data")
+                        res.sendStatus(response);
                     })
                     .catch(function (err) {
                         if (err.response) {
                             // console.log(err.response);
                             let { status, statusText } = err.response;
-                            console.log(status, statusText);
+                            console.log("inside post data catch")
+                            // console.log(status, statusText);
                             let json = { err_code: status, err_message: statusText };
-                            console.log(json);
-                            res.sendStatus(401).send(json);
+                            // console.log(json);
+                            res.status(401).send(json);
                         }
                     });
             }
@@ -54,81 +61,93 @@ app.post("/myServer", function (req, res) {
         else {
             axios.post(body.fetchURL, body.data)
                 .then(function (response) {
-                    // console.log(response);
-                    res.send(response.data);
+                    console.log(response.data);
+                    res.sendStatus(response);
 
                 })
                 .catch(function (err) {
                     if (err.response) {
                         // console.log(err.response);
                         let { status, statusText } = err.response;
-                        console.log(status, statusText);
+                        console.log("inside normal post catch")
+                        // console.log(status, statusText);
                         let json = { err_code: status, err_message: statusText };
-                        console.log(json);
-                        res.sendStatus(401).send(json);
+                        // console.log(json);
+                        res.status(401).send(json);
                     }
                 });
         }
     }
+
+
     //get request
     if (body.method === "GET") {
         if (body.fetchURL === (baseUrl + "/students")) {
             console.log(body.header);
-            let value = body.header.find(ele=>ele.key1.toLowerCase()==="authorization").value1;
-            if (!value)
-                res.sendStatus(401).send({ err_code: 401, err_message: "No token found. Provide a valid token" });
+            let ele = body.header.find(ele => {
+                if (ele.key) {
+                    console.log(ele.key, ele.value);
+                    return ele.key.toLowerCase() === "authorization";
+                }
+            });
+            if (!ele.value)
+                res.status(401).send({ err_code: 401, err_message: "No token found. Provide a valid token" });
             else {
-            axios.get(body.fetchURL, { headers: { authorization: value } })
+                axios.get(body.fetchURL, { headers: { authorization: ele.value } })
+                    .then(function (response) {
+                        console.log(response);
+                        res.sendStatus(response);
+                    })
+                    .catch(function (err) {
+                        if (err.response) {
+                            let { status, statusText } = err.response;
+                            console.log("inside get students catch")
+                            // console.log(status, statusText);
+                            let json = { err_code: status, err_message: statusText };
+                            // console.log(json);
+                            res.status(401).send(json);
+                        }
+                    });
+            }
+        }
+        else if (body.fetchURL === (baseUrl + "/getToken")) {
+            console.log("inside base url get token");
+            axios.get(body.fetchURL)
                 .then(function (response) {
-                    console.log(response);
-                    res.send(response.data);
+                    // console.log(response);
+                    console.log("inside get token then")
+                    res.sendStatus(response);
                 })
                 .catch(function (err) {
                     if (err.response) {
                         let { status, statusText } = err.response;
-                        console.log(status, statusText);
+                        console.log("inside get token catch")
+                        // console.log(status, statusText);
                         let json = { err_code: status, err_message: statusText };
-                        console.log(json);
-                        res.sendStatus(401).send(json);
+                        // console.log("inside getToken");
+                        res.status(401).send(json);
                     }
                 });
-            }
         }
-        else
-            if (body.fetchURL === (baseUrl + "/getToken")) {
-                console.log("inside base url get token");
-                axios.get(body.fetchURL)
-                    .then(function (response) {
-                        res.send(response.data);
-                    })
-                    .catch(function (err) {
-                        if (err.response) {
-                            let { status, statusText } = err.response;
-                            console.log(status, statusText);
-                            let json = { err_code: status, err_message: statusText };
-                            console.log(json);
-                            res.sendStatus(401).send(json);
-                        }
-                    });
-            }
-            else {
-                axios.get(body.fetchURL)
-                    .then(function (response) {
-                        // console.log("inside get url axios");
-                        // console.log(response.data);
-                        res.send(response.data);
+        else {
+            axios.get(body.fetchURL)
+                .then(function (response) {
+                    console.log("inside get url axios");
+                    // console.log(response.d);
+                    res.sendStatus(response);
 
-                    })
-                    .catch(function (err) {
-                        if (err.response) {
-                            let { status, statusText } = err.response;
-                            console.log(status, statusText);
-                            let json = { err_code: status, err_message: statusText };
-                            console.log(json);
-                            res.sendStatus(401).send(json);
-                        }
-                    });
-            }
+                })
+                .catch(function (err) {
+                    if (err.response) {
+                        let { status, statusText } = err.response;
+                        console.log("inside normal get fetchURl");
+                        // console.log(status, statusText);
+                        let json = { err_code: status, err_message: statusText };
+
+                        res.status(401).send(json);
+                    }
+                });
+        }
     }
 
 })
